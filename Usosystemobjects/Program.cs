6703292fa0e01cf39014cda99f37131f11720cbd
David Paez <jeffersonpaez5@gmail.com>
2025-07-04 18:20:48 -0400

    Add project files.

---
+++ Usosystemobjects/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Usosystemobjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //TODO Creacion de instancia Persona
+
+            Persona persona = new Persona();
+            persona.Nombre = "Petronila";
+
+            //Uso de metodos de System.Object
+
+            Console.WriteLine("ToString(): " + persona.ToString());
+            Console.WriteLine("GetType(): " + persona.GetType());
+            Console.WriteLine("GetHashCode(): " + persona.GetHashCode());
+
+            //Vamos a comparar con otros objetos
+
+            Persona otraPersona = new Persona();
+
+            Console.WriteLine("Equals(otraPersona): " + persona.Equals(otraPersona));
+            Console.ReadLine();
+
+        }
+
+        public class Persona
+        {
+            public string Nombre { get; set; }
+
+            public override string ToString()
+            {
+                return $"Persona: {Nombre} ";
+            }
+        }
+    }
+}
